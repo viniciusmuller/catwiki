af961eda25282bc793df4659b5f546a7675ea0bf
arcticlimer <vinigm.nho@gmail.com>
2021-02-11 16:30:20 -0300

    Update schema

--- backend/lib/cat_wiki_api/cats/breed.ex
+++ backend/lib/cat_wiki_api/cats/breed.ex
@@ -3,11 +3,30 @@ defmodule CatWikiAPI.Cats.Breed do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @schema ~w(
+    views image_url description temperament origin
+    lifespan adaptability affection_level child_friendly
+    grooming intelligence health_issues social_needs
+    stranger_friendly
+  )a
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "breeds" do
-    field :breed, :string
-    field :views, :integer, default: 1
+    field :views, :integer, default: 0
+    field :image_url, :string
+    field :description, :string
+    field :temperament, :string
+    field :origin, :string
+    field :lifespan, :string
+    field :adaptability, :integer
+    field :affection_level, :integer
+    field :child_friendly, :integer
+    field :grooming, :integer
+    field :intelligence, :integer
+    field :health_issues, :integer
+    field :social_needs, :integer
+    field :stranger_friendly, :integer
 
     timestamps()
   end
@@ -15,8 +34,8 @@ defmodule CatWikiAPI.Cats.Breed do
   @doc false
   def changeset(breed, attrs) do
     breed
-    |> cast(attrs, [:breed, :views])
-    |> validate_required([:breed, :views])
-    |> unique_constraint(:breed)
+    |> cast(attrs, @schema)
+    |> validate_required(@schema)
+    |> unique_constraint(:name)
   end
 end
--- backend/priv/repo/migrations/20210209190553_create_breeds.exs
+++ backend/priv/repo/migrations/20210209190553_create_breeds.exs
@@ -4,12 +4,25 @@ defmodule CatWikiAPI.Repo.Migrations.CreateBreeds do
   def change do
     create table(:breeds, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :breed, :string, null: false
       add :views, :integer
+      add :name, :string
+      add :image_url, :string
+      add :description, :string
+      add :temperament, :string
+      add :origin, :string
+      add :lifespan, :string
+      add :adaptability, :integer
+      add :affection_level, :integer
+      add :child_friendly, :integer
+      add :grooming, :integer
+      add :intelligence, :integer
+      add :health_issues, :integer
+      add :social_needs, :integer
+      add :stranger_friendly, :integer
 
       timestamps()
     end
 
-    create unique_index(:breeds, [:breed])
+    create unique_index(:breeds, [:name])
   end
 end
