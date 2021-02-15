7943292495d4836a2592caa900829d9e39501058
arcticlimer <vinigm.nho@gmail.com>
2021-02-14 21:54:36 -0300

    Remove unused socket channel

--- backend/lib/cat_wiki_api_web/endpoint.ex
+++ backend/lib/cat_wiki_api_web/endpoint.ex
@@ -10,10 +10,6 @@ defmodule CatWikiAPIWeb.Endpoint do
     signing_salt: "CHutHSJt"
   ]
 
-  socket "/socket", CatWikiAPIWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
